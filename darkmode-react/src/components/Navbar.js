b09e2c2298d8210d86a36ec8e25398faa1d27a68
rbiden <rabiestrella@gmail.com>
2023-01-10 09:31:05 +0800

    Optimize

--- darkmode-react/src/components/Navbar.js
+++ darkmode-react/src/components/Navbar.js
@@ -18,16 +18,16 @@ export const Navbar = () => {
                 <a href="#" className="font-medium hover:font-semibold">Portfolio</a>
                 <a href="#" className="font-medium hover:font-semibold">Contact</a>
                 <Button custom="mr-11" theme={ `${ darkMode ? 'dark' : 'light' }`}>Login</Button>
+                <Button custom="absolute right-1" onClick={ toggleDarkMode }>
+                    <MdDarkMode size={ 28 }/>
+                </Button>
             </div>
-            <Button custom="absolute right-1" onClick={ toggleDarkMode }>
-                <MdDarkMode size={ 28 }/>
-            </Button>
-            <Button custom='block mr-7 mobile-l:hidden' theme={ `${ darkMode ? 'dark' : 'light' }`} onClick={() => setIsOpen(!isOpen)}>
+            <Button custom='block mobile-l:hidden' theme={ `${ darkMode ? 'dark' : 'light' }`} onClick={() => setIsOpen(!isOpen)}>
                 <GoThreeBars size={ 28 }/>
             </Button>
             {isOpen && ( <div className="fixed inset-0 bg-dark opacity-75 mobile-l:hidden" onClick={() => setIsOpen(false)} /> )}
             <div className={`${isOpen ? 'block' : 'hidden'} ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'} fixed top-0 right-0 bottom-0 w-64 shadow-2xl overflow-y-auto mobile:hidden`}>
-                <div className={`${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'} flex items-center justify-between p-5`}>
+                <div className={`${darkMode ? 'bg-light text-dark' : 'bg-dark text-light'} flex items-center justify-between shadow-2xl p-5`}>
                     <h3 className="text-xl font-semibold">Menu</h3>
                     <button
                         className="text-gray-500 hover:text-gray-300 focus:outline-none focus:text-gray-300"
@@ -42,7 +42,10 @@ export const Navbar = () => {
                     <a href="#" className="block pl-3 py-5 mx-2 border-b-2 border-border hover:font-semibold">Portfolio</a>
                     <a href="#" className="block pl-3 py-5 mx-2 border-b-2 border-border hover:font-semibold">Contact</a>
                 </div>
-                <Button theme={ `${ darkMode ? 'dark' : 'light' }`} custom='absolute bottom-5 w-52 ml-6 text-lg'>Login</Button>
+                <Button theme={ `${ darkMode ? 'dark' : 'light' }`} custom='absolute bottom-5 w-40 ml-6 text-lg'>Login</Button>
+                <Button custom="absolute bottom-5 right-0" onClick={ toggleDarkMode }>
+                    <MdDarkMode size={ 28 }/>
+                </Button>
             </div>
         </nav>
     );
